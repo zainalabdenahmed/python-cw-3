a9236e50d1b41c8a5846e69511f0b98e712b9733
zainalabdenahmed <143084453+zainalabdenahmed@users.noreply.github.com>
2023-08-28 20:02:31 +0300

    aass

--- main.py
+++ main.py
@@ -1,1 +1,53 @@
-# write your code here
+favorite_animals = ['dog' ,' cat' ,'monkey' , 'rabbit']
+print(favorite_animals)
+favorite_animals.remove ('monkey')
+favorite_animals.append ('pingwin')
+for ss in favorite_animals:
+    print(f"I LOVE {ss}")
+
+
+
+numbers = [1,2,3,4,5]
+numbers_sum = 0
+for num in numbers:
+    numbers_sum+=num
+
+print(numbers_sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
